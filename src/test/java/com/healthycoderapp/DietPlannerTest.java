3dc1cdf94fc9a38e9eb3b5cabf664b0d8ba5d1b1
anastasiamexa <anastasia.mexa@yahoo.gr>
2024-01-19 13:32:56 +0200

    Learned about @BeforeEach and @AfterEach.

---
+++ src/test/java/com/healthycoderapp/DietPlannerTest.java
@@ -0,0 +1,44 @@
+package test.java.com.healthycoderapp;
+
+import main.java.com.healthycoderapp.Coder;
+import main.java.com.healthycoderapp.DietPlan;
+import main.java.com.healthycoderapp.DietPlanner;
+import main.java.com.healthycoderapp.Gender;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class DietPlannerTest {
+    private DietPlanner dietPlanner;
+
+    // Runs before each test
+    @BeforeEach
+    void setup() {
+        this.dietPlanner = new DietPlanner(20, 30, 50);
+    }
+
+    // Runs after each test
+    @AfterEach
+    void afterEach() {
+        System.out.println("A unit test was finished.");
+    }
+
+    @Test
+    void should_ReturnCorrectDietPlan_When_CorrectCoder() {
+        // given
+        Coder coder = new Coder(1.82, 75.0, 26, Gender.MALE);
+        DietPlan expected = new DietPlan(2202, 110, 73, 275);
+
+        // when
+        DietPlan actual = dietPlanner.calculateDiet(coder);
+
+        // then
+        assertAll("dietPlan",
+                () -> assertEquals(expected.getCalories(), actual.getCalories()),
+                () -> assertEquals(expected.getProtein(), actual.getProtein()),
+                () -> assertEquals(expected.getFat(), actual.getFat()),
+                () -> assertEquals(expected.getCarbohydrate(), actual.getCarbohydrate())
+        );
+    }
+}
